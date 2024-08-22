fa182abe1bc864b21d90c8d3f8671467d5cdeb88
ShreenidhiMv <shreenidhi.mv2@in.bosch.com>
2024-08-22 12:23:30 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,71 +1,53 @@
 #include <iostream>
-#include <cassert>
-#include <string>
+#include <assert.h>
+#include <sstream>
 
-std::string getColorPair(int majorIndex, int minorIndex) {
+int printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-    return std::to_string(majorIndex * 5 + minorIndex) + " | " + majorColor[majorIndex] + " | " + minorColor[minorIndex];
-}
-
-void printColorMap() {
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
-            std::cout << getColorPair(i, j) << "\n";
+    int i = 0, j = 0;
+    for(i = 0; i < 5; i++) {
+        for(j = 0; j < 5; j++) {
+            std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
         }
     }
+    return i * j;
 }
 
-void testColorPair() {
-    assert(getColorPair(0, 0) == "0 | White | Blue");
-    assert(getColorPair(0, 1) == "1 | White | Orange");
-    assert(getColorPair(1, 0) == "5 | Red | Blue");
-    assert(getColorPair(2, 4) == "14 | Black | Slate");
-    assert(getColorPair(4, 4) == "24 | Violet | Slate");
-}
+void testColorPairs() 
+{
+    std::ostringstream capturedOutput;
+    std::streambuf* oldCoutBuffer = std::cout.rdbuf(capturedOutput.rdbuf()); 
+    
+    printColorMap(); 
+    std::cout.rdbuf(oldCoutBuffer); 
+    
+    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+    
+    std::istringstream iss(capturedOutput.str());
+    std::string line;
+    int lineCount = 0;
 
-void testPrintColorMap() {
-    std::string expectedOutput =
-        "0 | White | Blue\n"
-        "1 | White | Orange\n"
-        "2 | White | Green\n"
-        "3 | White | Brown\n"
-        "4 | White | Slate\n"
-        "5 | Red | Blue\n"
-        "6 | Red | Orange\n"
-        "7 | Red | Green\n"
-        "8 | Red | Brown\n"
-        "9 | Red | Slate\n"
-        "10 | Black | Blue\n"
-        "11 | Black | Orange\n"
-        "12 | Black | Green\n"
-        "13 | Black | Brown\n"
-        "14 | Black | Slate\n"
-        "15 | Yellow | Blue\n"
-        "16 | Yellow | Orange\n"
-        "17 | Yellow | Green\n"
-        "18 | Yellow | Brown\n"
-        "19 | Yellow | Slate\n"
-        "20 | Violet | Blue\n"
-        "21 | Violet | Orange\n"
-        "22 | Violet | Green\n"
-        "23 | Violet | Brown\n"
-        "24 | Violet | Slate\n";
-
-    std::string actualOutput;
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
-            actualOutput += getColorPair(i, j) + "\n";
+    while (std::getline(iss, line)) 
+    {
+        int expectedPairNumber = lineCount;
+        std::string expectedOutput = std::to_string(expectedPairNumber) + " | " + majorColor[lineCount / 5] + " | " + minorColor[lineCount % 5];
+        
+        if (line != expectedOutput) 
+        {
+            std::cerr << "Test failed at index: " << lineCount << "\nActual: " << line << "\nExpected: " << expectedOutput << "\n";
+            assert(false && "Mismatch in color pair output");
         }
+        
+        lineCount++;
     }
-    assert(actualOutput != expectedOutput);
 }
 
 int main() {
-    testColorPair();
-    testPrintColorMap();
-    printColorMap();
+    int result = printColorMap();
+    testColorPairs();
+    assert(result == 25);
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
-
